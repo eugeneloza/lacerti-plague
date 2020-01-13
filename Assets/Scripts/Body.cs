a77a374a64014b6c89b03b0a8be8ae4da2910e71
Eugene Loza <zhloza@gmail.com>
2020-01-13 23:40:43 +0200

    Reduce number of bodyparts from 13 to 8

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -13,17 +13,17 @@ namespace Body
   /* Humanoid body, also note that only humanoids can wear clothes */
   public class THumanoidBody: TAbstractBody
   {
-	/* Humanoid bodyparts, total 13 pieces */
+	/* Humanoid bodyparts, total 8 pieces */
     public enum TBodyparts
 	{
 	  Head,
 	  LArm, RArm,
-	  LHand, RHand, //maybe leave only arms
-	  LChest, RChest, //maybe leave only chest
+	  //LHand, RHand, //maybe leave only arms
+	  Chest, //LChest, RChest, //maybe leave only chest
 	  Belly,
 	  Bottom,
 	  LLeg, RLeg,
-	  LFoot, RFoot //maybe leave only legs
+	  //LFoot, RFoot //maybe leave only legs
 	};	  
 	 /* CONSTRUCTOR */
 	 public THumanoidBody()
@@ -31,16 +31,11 @@ namespace Body
 	    bodyparts[(int)TBodyparts.Head] = new TBodypart();
 	    bodyparts[(int)TBodyparts.LArm] = new TBodypart();
 	    bodyparts[(int)TBodyparts.RArm] = new TBodypart();
-	    bodyparts[(int)TBodyparts.LHand] = new TBodypart();
-	    bodyparts[(int)TBodyparts.RHand] = new TBodypart();
-	    bodyparts[(int)TBodyparts.LChest] = new TBodypart();
-	    bodyparts[(int)TBodyparts.RChest] = new TBodypart();
+	    bodyparts[(int)TBodyparts.Chest] = new TBodypart();
 	    bodyparts[(int)TBodyparts.Belly] = new TBodypart();
 	    bodyparts[(int)TBodyparts.Bottom] = new TBodypart();
 	    bodyparts[(int)TBodyparts.LLeg] = new TBodypart();
 	    bodyparts[(int)TBodyparts.RLeg] = new TBodypart();
-	    bodyparts[(int)TBodyparts.LFoot] = new TBodypart();
-	    bodyparts[(int)TBodyparts.RFoot] = new TBodypart();
      }
   }
   
