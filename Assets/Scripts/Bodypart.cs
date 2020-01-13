cb94f533da5b2bd307f8b1881fc584ec72f2b57f
Eugene Loza <zhloza@gmail.com>
2020-01-13 23:33:06 +0200

    Add muscular tissues

--- Assets/Scripts/Bodypart.cs
+++ Assets/Scripts/Bodypart.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
-using Skin;
 using DamageType;
+using Skin;
+using Muscle;
 
 namespace Bodypart
 {
 	public class TBodypart
 	{
 	  /* Clothes and biological layers in/over this bodypart */
-	  //TClothesLayers clothesLayers;
+	  //TClothesLayers clothesLayers; //let Body handle clothes!
 	  TSkin skin;
-	  //TMuscle muscle;
+	  TMuscle muscle;
 	  //TBone bone;
 	  //TInternal internal;
 	  public void Damage(float dam, TDamageType damType)
@@ -17,7 +18,7 @@ namespace Bodypart
 		float remainingDamage = dam;
 		//remainingDamage = clothesLayers.Damage(remainingDamage, damType);
 		remainingDamage = skin.Damage(remainingDamage, damType);
-		//remainingDamage = muscle.Damage(remainingDamage, damType);
+		remainingDamage = muscle.Damage(remainingDamage, damType);
 		//remainingDamage = bone.Damage(remainingDamage, damType);
 		//remainingDamage = internal.Damage(remainingDamage, damType);
 		Debug.Assert(remainingDamage <= 0, "ERROR: Bodypart must completely absorb all incoming damage!"); //this can normally happen if internal organ or bone is destroyed
---
+++ Assets/Scripts/Muscle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Damagable;
+
+namespace Muscle
+{
+  /* Muscular tissues */
+  public class TMuscle: TInfectable
+  {
+	/* CONSTRUCTOR */
+	public TMuscle()
+	{
+	  MaxMaxDurability = 100f;
+	  absorbDamage[(int)TDamageType.Blunt] = 3f;
+	  absorbDamage[(int)TDamageType.Scratch] = 1f;
+	  absorbDamage[(int)TDamageType.Cut] = 0f;
+	  absorbDamage[(int)TDamageType.Piercing] = 5f;
+	  
+	  damageResist[(int)TDamageType.Blunt] = 0.5f;
+	  damageResist[(int)TDamageType.Scratch] = 1.0f;
+	  damageResist[(int)TDamageType.Cut] = 0.7f;
+	  damageResist[(int)TDamageType.Piercing] = 0.5f;
+    }    
+  }
+}
