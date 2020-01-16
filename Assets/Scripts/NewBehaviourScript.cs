f2ca1afdf183a66cdc1018a8b06d1005abbcbae1
Eugene Loza <zhloza@gmail.com>
2020-01-16 14:39:55 +0200

    Fix mono behavior script

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Bodypart;
+using Body;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //World.Update; //should pass internal world "time" (? use Time.deltaTime to make update semi-realtime)
+  }
 }
