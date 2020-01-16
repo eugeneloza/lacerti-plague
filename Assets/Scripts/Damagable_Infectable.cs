ed008d7c8c30d324e0a72ec55747cb64db29c9df
Eugene Loza <zhloza@gmail.com>
2020-01-16 22:59:23 +0200

    Manage TInfectable WIP

--- Assets/Scripts/Damagable_Infectable.cs
+++ Assets/Scripts/Damagable_Infectable.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using DamageType;
+using System;
+using InfectionType;
 
 namespace Damagable
 {
@@ -8,14 +9,39 @@ namespace Damagable
     /* Object that can get infected, such as muscles or internal organs */
     public abstract class TInfectable : TDamagable
     {
-      //todo
+      /* Current values of the infection in this infectable object */
+      public float[] infection;
+      public float[] infectionGrowthRate;
+      /* Each Lacerti infection requires a "construction material" to be built from
+         most common is free carbon, which is obtained with every food type
+         more hard-to-get is free ferrum, which is (for now) obtained only by drinking blood */
+      public float[] freeConstructionMaterial;
       public override void Update(float deltaTime)
       {
         base.Update(deltaTime);
-        //todo
+        foreach (TInfectionType i in (TInfectionType[]) Enum.GetValues(typeof(TInfectionType)))
+        {
+          if (i != TInfectionType.Disease)
+          {
+            //infection growth is calculated based on free metals in the body
+            float infectionGrowth = infection[(int)i] * infectionGrowthRate[(int)i] * deltaTime;
+            infection[(int)i] += infectionGrowth;
+          }
+          else
+          {
+            //normal disease just grows (we need some immunity to counter?
+            infection[(int)i] += infection[(int)i] * infectionGrowthRate[(int)i] * deltaTime;
+          }
+        }
       }
       /* CONSTRUCTOR */
-      public TInfectable() : base() {}
+      public TInfectable() : base()
+      {
+        //others are zero-initialized, which is fine for e.g. Argentumis, which are never supposed to be "owned" by player
+        infectionGrowthRate[(int)TInfectionType.Disease] = 0.01f;
+        infectionGrowthRate[(int)TInfectionType.Carbonis] = 0.01f;
+        infectionGrowthRate[(int)TInfectionType.Ferrumis] = 0.01f;
+      }
     }
   }
 }
